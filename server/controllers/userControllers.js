d54516eae4008bc077f1679450d9066394f79f02
Jesse Wowczuk <jessewowczuk@gmail.com>
2024-04-03 18:47:43 -0700

    verfing user token updated

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -76,8 +76,21 @@ const loginUser = async (req, res) => {
   }
 }
 
+const getProfile = (req, res) => {
+const {token} = req.cookies
+  if(token) {
+    jwt.verify(token, process.env.JWT_SECRET, {}, (err, user) => {
+      if (err) throw err;
+      res.json(user)
+    })
+  } else {
+    res.json(null)
+  }
+}
+
 module.exports = {
   test,
   registerUser,
   loginUser,
+  getProfile,
 }
